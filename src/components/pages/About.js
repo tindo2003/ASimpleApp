239e28505c0cea97cafe2793e2c35ffd68822a4f
Tin Do <tindo@sas.upenn.edu>
2022-09-09 21:49:35 -0400

    Commit message

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ import '../About.css'
 export default function About() {
     return (
         <div className="about--text">
-            <p>This project is made for consideration by the Penn Spark board. 
+            <p>My name is Tin Do. This project is made for consideration by the Penn Spark board. 
             Much thanks to <a href="https://scrimba.com/"> Scrimba </a> 
              and <a href="https://www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A" target="_blank">Brian Design</a> for React tutorial.
             
